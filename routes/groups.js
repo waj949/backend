807d2db50ae47bfab56a44814c8c4b44499e0cb3
rue-elwahishi <rubawahishi@gmail.com>
2020-01-17 12:23:24 +0100

    leave group and delete group functions added

--- routes/groups.js
+++ routes/groups.js
@@ -1,30 +1,10 @@
-const Router = (module.exports = require('express').Router())
-const Group = require("../models/group");
-const Friendship = require("../models/friendship.js");
-const ObjectId = require("mongodb").ObjectID
+const Router = (module.exports = require("express").Router());
 const AUTH = require("passport").authenticate("jwt", { session: false });
-Router.route('/').get(AUTH, (req, res) => {
-    Group.find({ users :  {$in : req.user._id}}).then( data => res.json(data))
-}).post(AUTH, (req, res) => {
-    req.body.admin = req.user._id
-    Group.create(req.body).then(data => {
-        Group.findByIdAndUpdate(data._id, { $push: { users: ObjectId(req.user._id)}}).then(data => {
-            
-            res.json(data)
-        });
+const groupControllers = require("../controllers/groupsController");
 
-    })
-})
-Router.route("/:id/add").post(AUTH, (req, res) => {
-    const group = req.params.id
-    const friend = req.user
-    const newMember = req.body.user
-    Friendship.exists({ $or : [{first: friend, second: newMember}, {first: newMember, second: friend}]}).then(data => {
-   if(!data) return res.json({success: false, msg:"you're not friends"})
-    Group.findByIdAndUpdate(group, {$push: {users: newMember}}).then(data => {
-        res.json({success: true})
-    })
-    })
-  
-  
-  })
+Router.route("/")
+  .get(AUTH, groupControllers.getGroups)
+  .post(AUTH, groupControllers.createGroup);
+
+Router.route("/:id/add").post(AUTH, groupControllers.addMember);
+Router.route("/:id/leave").get(AUTH, groupControllers.leaveGroup);
