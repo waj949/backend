01ca38d1390d25d8cfd446b2d2ba1eed6d90b05a
rue-elwahishi <rubawahishi@gmail.com>
2020-01-18 17:56:41 +0100

    getGroup function added and some fixes

--- models/message.js
+++ models/message.js
@@ -1,17 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-module.exports = mongoose.model("message" , mongoose.Schema({
-    content : String,
-    sender : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "user"
+module.exports = mongoose.model(
+  "message",
+  mongoose.Schema({
+    content: String,
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user"
     },
-    receiver : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "user"
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user"
     },
-    group : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "group" 
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "group"
     }
-}))
+  })
+);
--- routes/groups.js
+++ routes/groups.js
@@ -6,14 +6,14 @@ const groupControllers = require("../controllers/groupsController");
  * @route /api/groups GET
  * @description return all the groups that a user is in
  * @access private
- * 
+ *
  * @route /api/groups POST
  * @description create a group in the database
  * @access private
  */
 Router.route("/")
-    .get(AUTH, groupControllers.getGroups)
-    .post(AUTH, groupControllers.createGroup);
+  .get(AUTH, groupControllers.getGroups)
+  .post(AUTH, groupControllers.createGroup);
 
 /**
  * @route /api/groups/:id/add POST
@@ -24,7 +24,8 @@ Router.route("/:id/add").post(AUTH, groupControllers.addMember);
 
 /**
  * @route /api/groups/:id/leave GET
- * @description removes a user from a group,if the admin is leaving removes the whole group 
+ * @description removes a user from a group,if the admin is leaving removes the whole group
  * @access private
  */
 Router.route("/:id/leave").get(AUTH, groupControllers.leaveGroup);
+Router.route("/:id").get(AUTH, groupControllers.getGroup);
